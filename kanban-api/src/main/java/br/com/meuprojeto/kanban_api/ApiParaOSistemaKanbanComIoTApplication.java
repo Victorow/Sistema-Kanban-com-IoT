2bca737fde6c64819bcd20a827dc111633d76fed
Victor Augusto Ferreira dos Santos <victoroow@gmail.com>
2025-09-16 16:21:50 -0300

    Aplicação Base

---
+++ kanban-api/src/main/java/br/com/meuprojeto/kanban_api/ApiParaOSistemaKanbanComIoTApplication.java
@@ -0,0 +1,13 @@
+package br.com.meuprojeto.kanban_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParaOSistemaKanbanComIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParaOSistemaKanbanComIoTApplication.class, args);
+	}
+
+}
---
+++ kanban-api/src/test/java/br/com/meuprojeto/kanban_api/ApiParaOSistemaKanbanComIoTApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuprojeto.kanban_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParaOSistemaKanbanComIoTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
